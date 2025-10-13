c1ff04ec7c8431b350407dd7b20c3d814b71e859
脆薯饼 <Virgil@nanocat.me>
2025-10-13 15:04:39 +0800

    优化地理信息处理，更新地址和ISO 3166代码的逻辑，删除中国地区的子区域代码

--- src/response.dev.js
+++ src/response.dev.js
@@ -127,27 +127,30 @@ Console.info(`FORMAT: ${FORMAT}`);
 									//Console.debug(`AppleDispatcher: ${JSON.stringify(AppleDispatcher, null, 2)}`);
 									body = GEOPDPlaceResponse.composite(body, AppleDispatcher, Settings);
 									body.displayRegion = Settings.GeoCountryCode === "AUTO" ? Caches.PEP?.GCC : (Settings.GeoCountryCode ?? "US");
-									body.placeResult = body.placeResult.map(result => {
-										result.component = result.component.map(component => {
-											component.value = component.value.map(value => {
-												if (value.iso3166Code?.countryCode === "CN") {
-													value.iso3166Code.countryCode = Settings.GeoCountryCode === "AUTO" ? Caches.PEP?.GCC : (Settings.GeoCountryCode ?? "US");
-												}
-												return value;
-											});
-											return component;
-										});
-										return result;
-									});
 									body.mapsResult = body.mapsResult.map(result => {
 										if (result.place)
 											result.place.component = result.place.component.map(component => {
-												component.value = component.value.map(value => {
-													if (value.iso3166Code?.countryCode === "CN") {
-														value.iso3166Code.countryCode = Settings.GeoCountryCode === "AUTO" ? Caches.PEP?.GCC : (Settings.GeoCountryCode ?? "US");
+												const type = component.type;
+												const value = component.value[0];
+												switch (type) {
+													case "ADDRESS": {
+														const address = value.address;
+														const localizedAddress = address.localizedAddress[0];
+														const structuredAddress = localizedAddress.address.structuredAddress;
+														if (structuredAddress?.countryCode === "CN") {
+															structuredAddress.countryCode = Settings.GeoCountryCode === "AUTO" ? Caches.PEP?.GCC : (Settings.GeoCountryCode ?? "US");
+														}
+														break;
 													}
-													return value;
-												});
+													case "ISO_3166_CODE": {
+														const iso3166Code = value.iso3166Code;
+														if (iso3166Code?.countryCode === "CN") {
+															iso3166Code.countryCode = Settings.GeoCountryCode === "AUTO" ? Caches.PEP?.GCC : (Settings.GeoCountryCode ?? "US");
+															delete iso3166Code.subdivisonCode; // 删除子区域代码，以简化地理信息处理
+														}
+														break;
+													}
+												}
 												return component;
 											});
 										return result;

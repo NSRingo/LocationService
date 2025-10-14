8e16cecbf590f7384187b4ec939813a4f945ad68
脆薯饼 <Virgil@nanocat.me>
2025-10-14 16:22:09 +0800

    更新地理编码参数，修正位置坐标以支持原始位置保留

--- src/request.dev.js
+++ src/request.dev.js
@@ -106,14 +106,15 @@ Console.info(`FORMAT: ${FORMAT}`);
 											*/
 											switch (body.analyticMetadata.appIdentifier) {
 												case "com.apple.news":
-													_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lat", 40.7484523);
-													_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lng", -73.9859028);
+													_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lat", 40.748441);
+													_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lng", -73.985664);
 													break;
 												default:
 													if (body?.placeRequestParameters?.reverseGeocodingParameters?.preserveOriginalLocation) {
-														_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lat", 40.7484523);
-														_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lng", -73.9859028);
+														_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lat", 40.748441);
+														_.set(body, "placeRequestParameters.reverseGeocodingParameters.extendedLocation[0].latLng.lng", -73.985664);
 													}
+
 											}
 											break;
 										case "REQUEST_TYPE_MAPS_HOME":
